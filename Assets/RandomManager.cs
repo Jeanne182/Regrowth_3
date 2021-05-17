feba74418f8c49084fa36f5d3d8adcbc64cf2c88
Jeanne182 <jeanne@perrin.me>
2021-05-17 16:20:47 +0200

    Random life bulbs

---
+++ Assets/RandomManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomManager : MonoBehaviour
+{
+    public static RandomManager instance { get; private set; }
+    
+    public bool lifeMustPop = false;
+
+    private void Awake()
+    {
+        if(instance != null){
+          Debug.LogWarning("Il y a plus d'un PlayerMovement");
+          return ;
+        }
+        instance = this;
+    }
+}
