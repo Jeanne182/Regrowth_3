64b27b28fe82e58190b624d1781dbf19c02dc3fa
Fanny <fannyjoseph@gmail.com>
2021-05-17 18:10:51 +0200

    WIlt

---
+++ Assets/Scripts/GetTransformed.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System;
+
+public class GetTransformed : MonoBehaviour
+{
+    public Animator animator;
+    public Transformation parent;
+
+    void Update(){
+      /*if(parent.wilt){
+          animator.SetTrigger("New Trigger");
+      }
+      if(!parent.wilt){
+
+          animator.SetTrigger("New Trigger 0");
+      }*/
+    }
+}
--- Assets/Scripts/Transformation.cs
+++ Assets/Scripts/Transformation.cs
@@ -4,14 +4,34 @@ public class Transformation : MonoBehaviour
 {
 
   public Animator animator;
-  public bool wilt = true;
+  public Animator animatorChild;
+  public GetTransformed child;
+  public bool wilt ;
+
+  /*public static Transformation instance ;
+
+  private void Awake()
+  {
+      if(instance != null){
+        Debug.LogWarning("Il y a plus d'un Transformation");
+        return ;
+      }
+      instance = this;
+  }*/
 
   private void OnCollisionEnter2D(Collision2D collision)
   {
-      if(collision.transform.CompareTag("Player"))
+      if(collision.transform.CompareTag("Player") && wilt)
       {
-        animator.SetTrigger("Wilt");
+        animator.SetTrigger("New Trigger 0");
+        animatorChild.SetTrigger("New Trigger 0");
         wilt = false;
       }
+      else if(collision.transform.CompareTag("Player") && !wilt){
+        animator.SetTrigger("New Trigger");
+        animatorChild.SetTrigger("New Trigger");
+        wilt = true;
+      }
+
   }
 }
